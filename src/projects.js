59ff8a13ebcbcfb1e8d9e351212137f42661eb46
Sasher Gurung <sashergrg602@gmail.com>
2026-02-13 13:37:01 +0545

    Made all Pages Responsive

--- src/projects.js
+++ src/projects.js
@@ -8,6 +8,22 @@ const projects = [
         tools: ["React", "JavaScript", "HTML", "CSS", "Git"],
         live: "",
         img: WorkInProgressImg
+    },
+    {
+        id: 2,
+        name: "Sneaker-Ecommerce Website (Ongoing)",
+        description: "Will be building a Sneaker-Ecommerce website using React for the frontend. The website will feature a user-friendly interface, allowing customers to browse and purchase sneakers online. It will include features such as product listings, shopping cart functionality.",
+        tools: ["React", "JavaScript", "HTML", "CSS", "Git"],
+        live: "",
+        img: WorkInProgressImg
+    },
+    {
+        id: 3,
+        name: "Sneaker-Ecommerce Website (Ongoing)",
+        description: "Will be building a Sneaker-Ecommerce website using React for the frontend. The website will feature a user-friendly interface, allowing customers to browse and purchase sneakers online. It will include features such as product listings, shopping cart functionality.",
+        tools: ["React", "JavaScript", "HTML", "CSS", "Git"],
+        live: "",
+        img: WorkInProgressImg
     }
 ]
 
