d13b4dd5efaa4a2d2cb70c2072042e3ed17cb8b9
Sasher Gurung <sashergrg602@gmail.com>
2026-02-14 10:29:22 +0545

    Update Projects

--- src/projects.js
+++ src/projects.js
@@ -8,22 +8,6 @@ const projects = [
         tools: ["React", "JavaScript", "HTML", "CSS", "Git"],
         live: "",
         img: WorkInProgressImg
-    },
-    {
-        id: 2,
-        name: "Sneaker-Ecommerce Website (Ongoing)",
-        description: "Will be building a Sneaker-Ecommerce website using React for the frontend. The website will feature a user-friendly interface, allowing customers to browse and purchase sneakers online. It will include features such as product listings, shopping cart functionality.",
-        tools: ["React", "JavaScript", "HTML", "CSS", "Git"],
-        live: "",
-        img: WorkInProgressImg
-    },
-    {
-        id: 3,
-        name: "Sneaker-Ecommerce Website (Ongoing)",
-        description: "Will be building a Sneaker-Ecommerce website using React for the frontend. The website will feature a user-friendly interface, allowing customers to browse and purchase sneakers online. It will include features such as product listings, shopping cart functionality.",
-        tools: ["React", "JavaScript", "HTML", "CSS", "Git"],
-        live: "",
-        img: WorkInProgressImg
     }
 ]
 
