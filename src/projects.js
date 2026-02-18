472fcb3c992ba7e077aec16d86c63d0ea62701b3
Sasher Gurung <sashergrg602@gmail.com>
2026-02-19 00:17:21 +0545

    Update Projects

--- src/projects.js
+++ src/projects.js
@@ -4,9 +4,9 @@ const projects = [
     {
         id: 1,
         name: "Sneaker-Ecommerce Website (Ongoing)",
-        description: "Will be building a Sneaker-Ecommerce website using React for the frontend. The website will feature a user-friendly interface, allowing customers to browse and purchase sneakers online. It will include features such as product listings, shopping cart functionality.",
-        tools: ["React", "JavaScript", "HTML", "CSS", "Git"],
-        live: "",
+        description: "Will be building a Sneaker-Ecommerce website using NextJS, React for the frontend. The website will feature a user-friendly interface, allowing customers to browse and purchase sneakers online. It will include features such as product listings, shopping cart functionality.",
+        tools: ["NextJs", "React", "JavaScript", "HTML", "CSS", "Git", "TailWind CSS"],
+        live: "https://lux-sole.vercel.app",
         img: WorkInProgressImg
     }
 ]
